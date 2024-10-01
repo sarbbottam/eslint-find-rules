582e9e4b39adb810c622bff4d4b776ae4855af7d
yutak23 <yuta.katayama.23@gmail.com>
2024-10-01 09:06:14 +0900

    fix: make corrections based on review findings second

--- test/lib/rule-finder.js
+++ test/lib/rule-finder.js
@@ -5,7 +5,7 @@ const proxyquire = require('proxyquire');
 const { builtinRules } = require('eslint/use-at-your-own-risk');
 const semver = require('semver');
 const eslintPkg = require('eslint/package.json');
-const merge = require('lodash/merge');
+const merge = require('lodash.merge');
 
 const processCwd = process.cwd;
 const isV8Eslint = semver.satisfies(eslintPkg.version, '< 9');
